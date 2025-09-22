382a56f152c41deeacadb07dd871d63f981241f6
fajarnr <fajar1700018007@webmail.uad.ac.id>
2025-09-22 22:52:38 +0700

    tampilan cekout

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::view('/', 'pages.homepage')->name('home');
 Route::view('/homeproduct', 'pages.homeproduct')->name('homeproduct');
+Route::view('/cekout', 'pages.cekout')->name('cekout');
 
 Route::view('profile', 'profile')
     ->middleware(['auth'])
